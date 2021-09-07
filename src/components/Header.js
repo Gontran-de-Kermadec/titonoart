9be2443c6e7740a1ac4f75ba87577eadf1abb4dc
Gontran de Kermadec <gontrandekermadec@gmail.com>
2021-09-07 21:02:22 +0200

    started delivery block

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,11 @@ function Header() {
 			.querySelector(".header__small-screen-menu")
 			.classList.toggle("visible");
 	};
+	const toggleDelivery = () => {
+		document.querySelectorAll(".delivery__block").forEach((element) => {
+			element.classList.toggle("displayed");
+		});
+	};
 	return (
 		<div>
 			<header role="banner">
@@ -65,11 +70,11 @@ function Header() {
 										</svg>
 									</a>
 									<svg
+										onClick={() => toggleDelivery()}
 										aria-hidden="true"
 										focusable="false"
 										data-prefix="fas"
 										data-icon="truck"
-										//class="svg-inline--fa fa-truck fa-w-20"
 										role="img"
 										xmlns="http://www.w3.org/2000/svg"
 										viewBox="0 0 640 512"
@@ -79,6 +84,12 @@ function Header() {
 											d="M624 352h-16V243.9c0-12.7-5.1-24.9-14.1-33.9L494 110.1c-9-9-21.2-14.1-33.9-14.1H416V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48v320c0 26.5 21.5 48 48 48h16c0 53 43 96 96 96s96-43 96-96h128c0 53 43 96 96 96s96-43 96-96h48c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zM160 464c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm320 0c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm80-208H416V144h44.1l99.9 99.9V256z"
 										></path>
 									</svg>
+									<div className="delivery__block">
+										<div className="arrow arrow__outer">
+											<div className="arrow arrow__inner"></div>
+										</div>
+										<p>Infos livraison</p>
+									</div>
 								</div>
 							</li>
 						</ul>
