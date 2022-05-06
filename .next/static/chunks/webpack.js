a70c67969f5025fb8a3088e89e8380d6623a8534
Gontran de Kermadec <gontrandekermadec@gmail.com>
2022-05-06 17:09:01 +0200

    css updated for nextjs on creation page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e2e4407fa0564e0"; }
+/******/ 		__webpack_require__.h = function() { return "6e25c7a4b033190a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/creation.js
+++ src/pages/creation.js
@@ -1,7 +1,12 @@
+import creationStyle from "../style/Creation.module.css";
+import Link from "next/link";
+
 export default function Creation() {
 	return (
-		<div className="creation">
-			<div className="creation__intro">
+		<div className={creationStyle.creation}>
+			{/* <div className="creation"> */}
+			<div className={creationStyle.creation__intro}>
+				{/* <div className="creation__intro"> */}
 				<h2>Créations</h2>
 				<p>
 					Toutes nos créations sont des réalisations uniques qui nécessitent de
@@ -10,49 +15,99 @@ export default function Creation() {
 					artisanale et à la création artistique.
 				</p>
 			</div>
-			{/* <div className="creation__container">
-				<Link to="/creation/art">
-					<div className="single__creation-card creation__card">
-						<figure>
-							<div className="creation__img-container">
+			<div className={creationStyle.creation__container}>
+				{/* <div className="creation__container"> */}
+				<Link href="/creation/art" passHref>
+					<a href="replace">
+						<div
+							className={
+								creationStyle.single__creation__card +
+								" " +
+								creationStyle.creation__card
+							}
+						>
+							{/* <div className="single__creation-card creation__card"> */}
+							<figure>
+								{/* <div className={creationStyle.creation__img__container}> */}
+								{/* <div className="creation__img-container"> */}
 								<img
-									src={voilier}
-									onMouseEnter={(e) => (e.currentTarget.src = terre1)}
-									onMouseLeave={(e) => (e.currentTarget.src = voilier)}
+									// src={voilier}
+									src="/images/voilier.jpg"
+									// onMouseEnter={(e) => (e.currentTarget.src = terre1)}
+									onMouseEnter={(e) =>
+										(e.currentTarget.src = "/images/terre1.jpg")
+									}
+									// onMouseLeave={(e) => (e.currentTarget.src = voilier)}
+									onMouseLeave={(e) =>
+										(e.currentTarget.src = "/images/voilier.jpg")
+									}
 									alt="tonneau d'un voilier"
 								></img>
-							</div>
-						</figure>
-						<button>Art</button>
-					</div>
+								{/* </div> */}
+							</figure>
+							<button>Art</button>
+						</div>
+					</a>
 				</Link>
-				<Link to="/creation/bar">
-					<div className="single__creation-card creation__card">
-						<figure>
-							<img
-								src={bar}
-								onMouseEnter={(e) => (e.currentTarget.src = bar2)}
-								onMouseLeave={(e) => (e.currentTarget.src = bar)}
-								alt="tonneau bar"
-							></img>
-						</figure>
-						<button>Bar</button>
-					</div>
+				<Link href="/creation/bar" passHref>
+					<a href="replace">
+						<div
+							className={
+								creationStyle.single__creation__card +
+								" " +
+								creationStyle.creation__card
+							}
+						>
+							{/* <div className="single__creation-card creation__card"> */}
+							<figure>
+								<img
+									//src={bar}
+									src="/images/bar.jpg"
+									onMouseEnter={(e) =>
+										(e.currentTarget.src = "/images/bar1.jpg")
+									}
+									// onMouseEnter={(e) => (e.currentTarget.src = bar2)}
+									onMouseLeave={(e) =>
+										(e.currentTarget.src = "/images/bar.jpg")
+									}
+									// onMouseLeave={(e) => (e.currentTarget.src = bar)}
+									alt="tonneau bar"
+								></img>
+							</figure>
+							<button>Bar</button>
+						</div>
+					</a>
 				</Link>
-				<Link to="/creation/deco">
-					<div className="single__creation-card creation__card">
-						<figure>
-							<img
-								src={papillon}
-								onMouseEnter={(e) => (e.currentTarget.src = pirate1)}
-								onMouseLeave={(e) => (e.currentTarget.src = papillon)}
-								alt="tonneau papillon"
-							></img>
-						</figure>
-						<button>Déco</button>
-					</div>
+				<Link href="/creation/deco" passHref>
+					<a href="replace">
+						<div
+							className={
+								creationStyle.single__creation__card +
+								" " +
+								creationStyle.creation__card
+							}
+						>
+							{/* <div className="single__creation-card creation__card"> */}
+							<figure>
+								<img
+									src="/images/papillon0.jpg"
+									// src={papillon}
+									onMouseEnter={(e) =>
+										(e.currentTarget.src = "/images/pirate1.jpg")
+									}
+									// onMouseEnter={(e) => (e.currentTarget.src = pirate1)}
+									onMouseLeave={(e) =>
+										(e.currentTarget.src = "/images/papillon0.jpg")
+									}
+									// onMouseLeave={(e) => (e.currentTarget.src = papillon)}
+									alt="tonneau papillon"
+								></img>
+							</figure>
+							<button>Déco</button>
+						</div>
+					</a>
 				</Link>
-			</div> */}
+			</div>
 		</div>
 	);
 }
